f3a40616037711fe26bb7fa79c6262ff10674e4b
dotslashf <fadhlurahman.i@gmail.com>
2022-03-25 13:42:25 +0800

    init next auth spotify

---
+++ services/spotify.js
@@ -0,0 +1,30 @@
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const basic = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+const CURRENTLY_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+
+const getAccessToken = async refresh_token => {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token,
+    }),
+  });
+
+  return response.json();
+};
+
+export const getCurrentlyPlaying = async refresh_token => {
+  const { access_token } = await getAccessToken(refresh_token);
+  return fetch(CURRENTLY_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
